82926363080c5a623f3a186e652c7cd872c9dae6
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-13 10:21:37 +0200

    Test that BlumPrime returns a safe prime
    Previously, we just tested that BlumPrime returned a prime, and not that
    this prime was 3 mod 4, or safe. This adjusts the test to also check
    that (p - 1) / 2 is a prime. This implies that p = 3 mod 4.

--- pkg/math/sample/sample_test.go
+++ pkg/math/sample/sample_test.go
@@ -12,6 +12,11 @@ func TestBlumPrime(t *testing.T) {
 	if !p.ProbablyPrime(blumPrimeProbabilityIterations) {
 		t.Error("BlumPrime generated a non prime number: ", p)
 	}
+	q := new(big.Int).Sub(p, new(big.Int).SetUint64(1))
+	q.Rsh(q, 1)
+	if !q.ProbablyPrime(blumPrimeProbabilityIterations) {
+		t.Error("p isn't safe because (p - 1) / 2 isn't prime", q)
+	}
 }
 
 // This exists to save the results of functions we want to benchmark, to avoid
