8befd117ccd9ea27e19e702cdbf629c6d44dd9b2
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-14 15:53:03 +0200

    Define BytesWithDomain type
    This is a useful wrapper allowing us to ascribe a specific domain to some
    piece of data. This allows us to have domain separation for different byte
    slices, without creating a wrapper type each time.
    
    That being said, it probably would be a good idea to define wrapper types
    for some of these, but I digress.

--- internal/writer/domain.go
+++ internal/writer/domain.go
@@ -37,3 +37,23 @@ func WriteWithDomain(w io.Writer, object WriterToWithDomain) (int64, error) {
 	total += int64(n)
 	return total, err
 }
+
+// BytesWithDomain is a useful wrapper to annotate some chunk of data with a domain.
+//
+// The intention is to wrap some data using this struct, and then call WriteWithDomain,
+// or use this struct as a WriterToWithDomain somewhere else.
+type BytesWithDomain struct {
+	TheDomain string
+	Bytes     []byte
+}
+
+// WriteTo implements io.WriterTo
+func (b BytesWithDomain) WriteTo(w io.Writer) (int64, error) {
+	n, err := w.Write(b.Bytes)
+	return int64(n), err
+}
+
+// Domain implements WriterToWithDomain.
+func (b BytesWithDomain) Domain() string {
+	return b.TheDomain
+}
