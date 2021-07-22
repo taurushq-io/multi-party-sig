e48d9ccd80cfa8f75c0ed11a7f77116734058ce7
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-22 15:24:48 +0200

    Implement mta with safenum

--- protocols/cmp/sign/mta_test.go
+++ protocols/cmp/sign/mta_test.go
@@ -29,8 +29,8 @@ func Test_newMtA(t *testing.T) {
 	bi := sample.Scalar(rand.Reader)
 	bj := sample.Scalar(rand.Reader)
 
-	Ki, _ := i.Paillier.Enc(bi.BigInt())
-	Kj, _ := j.Paillier.Enc(bj.BigInt())
+	Ki, _ := i.Paillier.Enc(bi.Int())
+	Kj, _ := j.Paillier.Enc(bj.Int())
 
 	aibj := curve.NewScalar().Multiply(ai, bj)
 	ajbi := curve.NewScalar().Multiply(aj, bi)
@@ -44,15 +44,14 @@ func Test_newMtA(t *testing.T) {
 
 	alpha_ij, err := skj.Dec(msg_i_to_j.D)
 	assert.NoError(t, err, "decryption should pass")
-	gamma_ij_int := alpha_ij.Sub(alpha_ij, mta_i_to_j.BetaNeg)
+	gamma_ij_int := alpha_ij.Add(alpha_ij, mta_i_to_j.BetaNeg.Clone().Neg(1), -1)
 	alpha_ji, err := ski.Dec(msg_j_to_i.D)
 	assert.NoError(t, err, "decryption should pass")
-	gamma_ji_int := alpha_ji.Sub(alpha_ji, mta_j_to_i.BetaNeg)
-	gamma_ij := curve.NewScalarBigInt(gamma_ij_int)
-	gamma_ji := curve.NewScalarBigInt(gamma_ji_int)
+	gamma_ji_int := alpha_ji.Add(alpha_ji, mta_j_to_i.BetaNeg.Clone().Neg(1), -1)
+	gamma_ij := curve.NewScalarInt(gamma_ij_int)
+	gamma_ji := curve.NewScalarInt(gamma_ji_int)
 	gamma := curve.NewScalar().Add(gamma_ij, gamma_ji)
 	assert.Equal(t, c, gamma, "a•b should be equal to α + β")
 	assert.True(t, msg_i_to_j.VerifyAffG(hash.New(), Ai, Kj, i, j, nil))
 	assert.True(t, msg_j_to_i.VerifyAffG(hash.New(), Aj, Ki, j, i, nil))
-
 }
