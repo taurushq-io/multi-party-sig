74979b97ae0bc913a30bb0ec582864393009b546
Adrian Hamelink <adrian.hamelink@gmail.com>
2021-07-22 19:46:01 +0200

    round.Helper: - Generate hash state in a more general way, and include the protocolID - add group information here - rename receivers - validation of parties

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"io"
+)
+
 // RoundNumber is the index of the current round.
 // Starts at zero
 type RoundNumber uint16
@@ -7,3 +11,14 @@ type RoundNumber uint16
 // ProtocolID defines a unique identifier for a protocol.
 // It will be written as {shorthand for paper}/{protocol function}-{version}-{broadcast type}
 type ProtocolID string
+
+// WriteTo implements io.WriterTo interface.
+func (pid ProtocolID) WriteTo(w io.Writer) (int64, error) {
+	n, err := w.Write([]byte(pid))
+	return int64(n), err
+}
+
+// Domain implements writer.WriterToWithDomain
+func (ProtocolID) Domain() string {
+	return "Protocol ID"
+}
