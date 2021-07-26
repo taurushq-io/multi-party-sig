c00bf489b11eba0fce480b7e49fff8bc1a3fce72
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-26 16:34:33 +0200

    Define Signature struct for Frost sign
    This will be returned later.

--- protocols/frost/sign/types.go
+++ protocols/frost/sign/types.go
@@ -1,6 +1,12 @@
 package sign
 
-import io "io"
+import (
+	io "io"
+
+	"github.com/taurusgroup/cmp-ecdsa/pkg/hash"
+	"github.com/taurusgroup/cmp-ecdsa/pkg/math/curve"
+	"github.com/taurusgroup/cmp-ecdsa/pkg/math/sample"
+)
 
 // messageHash is a wrapper around bytes to provide some domain separation
 type messageHash []byte
@@ -14,4 +20,34 @@ func (m messageHash) WriteTo(w io.Writer) (int64, error) {
 // Domain implements WriterToWithDomain, and separates this type within hash.Hash.
 func (messageHash) Domain() string {
 	return "messageHash"
+}
+
+// Signature represents the result of a Schnorr signature.
+//
+// This signature claims to satisfy:
+//
+//    z * G = R + H(R, Y, m) * Y
+//
+// for a public key Y.
+type Signature struct {
+	// R is the commitment point.
+	R *curve.Point
+	// z is the response scalar.
+	z *curve.Scalar
+}
+
+// Verify checks if a signature equation actually holds.
+//
+// Note that m is the hash of a message, and not the message itself.
+func (sig Signature) Verify(public *curve.Point, m []byte) bool {
+	challengeHash := hash.New()
+	challengeHash.WriteAny(sig.R, public, messageHash(m))
+	challenge := sample.Scalar(challengeHash)
+
+	expected := curve.NewIdentityPoint().ScalarMult(challenge, public)
+	expected.Add(expected, sig.R)
+
+	actual := curve.NewIdentityPoint().ScalarBaseMult(sig.z)
+
+	return expected.Equal(actual)
 }
