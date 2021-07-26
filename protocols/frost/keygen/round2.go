2fa4ba703ae9d3ebcdfd76d7f48d4ff8b68df8f2
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-26 09:06:42 +0200

    Implement ProcessMessage for Round2

--- protocols/frost/keygen/round2.go
+++ protocols/frost/keygen/round2.go
@@ -1,11 +1,14 @@
 package keygen
 
 import (
+	"fmt"
+
 	"github.com/taurusgroup/cmp-ecdsa/pkg/math/polynomial"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/message"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/party"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/round"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/types"
+	zksch "github.com/taurusgroup/cmp-ecdsa/pkg/zk/sch"
 )
 
 // This round corresponds with steps 5 of Round 1, 1 of Round 2, Figure 1 in the Frost paper:
@@ -21,7 +24,35 @@ type round2 struct {
 	Phi map[party.ID]*polynomial.Exponent
 }
 
-func (r *round2) ProcessMessage(party.ID, message.Content) error { return nil }
+// ProcessMessage implements round.Round.
+func (r *round2) ProcessMessage(l party.ID, content message.Content) error {
+	msg, ok := content.(*Keygen2)
+	if !ok {
+		return fmt.Errorf("failed to convert message to Keygen2: %v", msg)
+	}
+
+	// These steps come from Figure 1, Round 1 of the Frost paper
+
+	// 5. "Upon receiving Phi_l, sigma_l from participants 1 <= l <= n, participant
+	// P_i verifies sigma_l = (R_l, mu_l), aborting on failure, by checking
+	// R_l = mu_l * G - c_l * phi_l0, where c_l = H(l, ctx, phi_l0, R_l).
+	//
+	// Upon success, participants delete { sigma_l | 1 <= l <= n }"
+	//
+	// Note: I've renamed C_l to Phi_l, as in the previous round.
+
+	r.Phi[l] = msg.Phi_i
+	R_l := msg.R_i
+	mu_l := msg.Mu_i
+	// To see why this is correct, compare this verification with the proof we
+	// produced in the previous round. Note how we do the same hash cloning,
+	// but this time with the ID of the message sender.
+	if !zksch.Verify(r.Helper.HashForID(l), R_l, r.Phi[l].Constant(), mu_l) {
+		return fmt.Errorf("failed to verify Schnorr proof for party %s", l)
+	}
+
+	return nil
+}
 
 func (r *round2) Finalize(out chan<- *message.Message) (round.Round, error) {
 	panic("unimplemented")
