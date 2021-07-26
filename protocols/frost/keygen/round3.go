855569785d51e281aae3bd1763872440eaf901f0
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-26 09:47:22 +0200

    Implement round3 struct for Frost keygen

--- protocols/frost/keygen/round3.go
+++ protocols/frost/keygen/round3.go
@@ -1,17 +1,34 @@
 package keygen
 
 import (
+	"fmt"
+
+	"github.com/taurusgroup/cmp-ecdsa/pkg/math/curve"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/message"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/party"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/round"
 	"github.com/taurusgroup/cmp-ecdsa/pkg/types"
 )
 
+// This round corresponds with steps 2-4 of Round 2, Figure 1 in the Frost paper:
+//   https://eprint.iacr.org/2020/852.pdf
 type round3 struct {
 	*round2
+
+	// shareFrom is the secret share sent to us by a given party, including ourselves.
+	//
+	// shareFrom[l] corresponds to f_l(i) in the Frost paper, with i our own ID.
+	shareFrom map[party.ID]*curve.Scalar
 }
 
 func (r *round3) ProcessMessage(l party.ID, content message.Content) error {
+	msg, ok := content.(*Keygen3)
+	if !ok {
+		return fmt.Errorf("failed to convert message to Keygen3: %v", msg)
+	}
+
+	r.shareFrom[l] = msg.F_li
+
 	return nil
 }
 
