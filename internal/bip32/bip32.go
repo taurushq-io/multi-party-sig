ea1d5d0f310d860c23ab031cb7f885c272bd26f7
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-02 15:10:42 +0200

    Implement scalar shift for BIP-32 internally

---
+++ internal/bip32/bip32.go
@@ -0,0 +1,48 @@
+package bip32
+
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/taurusgroup/cmp-ecdsa/pkg/math/curve"
+)
+
+func compressPoint(p *curve.Point) []byte {
+	out := make([]byte, 33)
+	out[0] = 3
+	if p.HasEvenY() {
+		out[0] = 2
+	}
+	copy(out[1:], p.XBytes()[:])
+	return out
+}
+
+// DeriveScalar uses a public point, chaining value, and index, to derive a scalar and chaining value.
+//
+// This scalar should be added to the secret key.
+//
+// If an error is returned, this means that this index will not be useable, and another
+// index should be used instead.
+//
+// This function will panic if an index for a hardened key is used.
+func DeriveScalar(public *curve.Point, chaining []byte, i uint32) (*curve.Scalar, []byte, error) {
+	if i>>31 != 0 {
+		panic("DeriveScalar doesn't work with hardened keys.")
+	}
+
+	h := hmac.New(sha512.New, chaining)
+	_, _ = h.Write(compressPoint(public))
+	iBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(iBytes, i)
+	h.Write(iBytes)
+
+	out := h.Sum(nil)
+	scalar, ok := curve.NewScalar().SetBytes(out[:32])
+	if !ok {
+		return nil, nil, fmt.Errorf("bad index: %d", i)
+	}
+
+	return scalar, out[32:], nil
+}
