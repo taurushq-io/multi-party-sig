5157fe11e01050de72c6d33b279477aa5eaf2675
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-09 13:32:45 +0200

    Sketch out basic Curve interfac

--- pkg/math/curve/curve.go
+++ pkg/math/curve/curve.go
@@ -1,6 +1,8 @@
 package curve
 
 import (
+	"encoding"
+
 	"github.com/cronokirby/safenum"
 	"github.com/decred/dcrd/dcrec/secp256k1/v3"
 )
@@ -8,3 +10,31 @@ import (
 // q is the order of the base point.
 var q = secp256k1.S256().Params().N
 var qMod = safenum.ModulusFromNat(new(safenum.Nat).SetBig(q, q.BitLen()))
+
+type Curve interface {
+	NewPoint() PointI
+	NewScalar() ScalarI
+}
+
+type ScalarI interface {
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
+	Add(ScalarI) ScalarI
+	Mul(ScalarI) ScalarI
+	Invert() ScalarI
+	Negate() ScalarI
+	Equal(ScalarI) bool
+	IsZero() bool
+	Set(ScalarI) ScalarI
+	SetNat(ScalarI)
+	Act(PointI) PointI
+	ActOnBase() PointI
+}
+
+type PointI interface {
+	encoding.BinaryMarshaler
+	encoding.BinaryUnmarshaler
+	Add(PointI) PointI
+	Negate(PointI) PointI
+	Equal(PointI) bool
+}
