293b32f1b94303510a9da252e47adb022aaa47ac
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-09-03 15:47:19 +0200

    Benchmark doerner setup

--- protocols/doerner/keygen/keygen_test.go
+++ protocols/doerner/keygen/keygen_test.go
@@ -1,6 +1,7 @@
 package keygen
 
 import (
+	"errors"
 	"sync"
 	"testing"
 
@@ -12,21 +13,14 @@ import (
 	"github.com/taurusgroup/multi-party-sig/pkg/protocol"
 )
 
-func checkOutput(t *testing.T, configSender *ConfigSender, configReceiver *ConfigReceiver) {
-	require.True(t, configSender.Public.Equal(configReceiver.Public))
-	require.False(t, configSender.Public.IsIdentity())
-	secret := configSender.Group().NewScalar().Set(configSender.SecretShare).Mul(configReceiver.SecretShare)
-	public := secret.ActOnBase()
-	require.True(t, public.Equal(configSender.Public))
-}
-
 func worker(wg *sync.WaitGroup, id party.ID, handler protocol.Handler, network *test.Network) {
 	defer wg.Done()
 	test.HandlerLoop(id, handler, network)
 }
 
-func TestKeygen(t *testing.T) {
-	var pl *pool.Pool
+func runDoerner() (*ConfigSender, *ConfigReceiver, error) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
 
 	group := curve.Secp256k1{}
 	partyIDs := test.PartyIDs(2)
@@ -34,23 +28,51 @@ func TestKeygen(t *testing.T) {
 
 	var wg sync.WaitGroup
 	h0, err := protocol.NewTwoPartyHandler(StartKeygen(group, true, partyIDs[0], partyIDs[1], pl), []byte("session"), true)
-	require.NoError(t, err, "round creation should not result in an error")
+	if err != nil {
+		return nil, nil, err
+	}
 	h1, err := protocol.NewTwoPartyHandler(StartKeygen(group, false, partyIDs[1], partyIDs[0], pl), []byte("session"), false)
-	require.NoError(t, err, "round creation should not result in an error")
+	if err != nil {
+		return nil, nil, err
+	}
 	wg.Add(2)
 	go worker(&wg, partyIDs[0], h0, network)
 	go worker(&wg, partyIDs[1], h1, network)
 	wg.Wait()
 
 	resultRound0, err := h0.Result()
-	require.NoError(t, err)
+	if err != nil {
+		return nil, nil, err
+	}
 	configReceiver, ok := resultRound0.(*ConfigReceiver)
-	require.True(t, ok)
+	if !ok {
+		return nil, nil, errors.New("failed to cast result to *ConfigReceiver")
+	}
 
 	resultRound1, err := h1.Result()
-	require.NoError(t, err)
+	if err != nil {
+		return nil, nil, err
+	}
 	configSender, ok := resultRound1.(*ConfigSender)
-	require.True(t, ok)
+	if !ok {
+		return nil, nil, errors.New("failed to cast result to *ConfigSender")
+	}
 
-	checkOutput(t, configSender, configReceiver)
+	return configSender, configReceiver, nil
+}
+
+func TestKeygen(t *testing.T) {
+	configSender, configReceiver, err := runDoerner()
+	require.NoError(t, err)
+	require.True(t, configSender.Public.Equal(configReceiver.Public))
+	require.False(t, configSender.Public.IsIdentity())
+	secret := configSender.Group().NewScalar().Set(configSender.SecretShare).Mul(configReceiver.SecretShare)
+	public := secret.ActOnBase()
+	require.True(t, public.Equal(configSender.Public))
+}
+
+func BenchmarkKeygen(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		runDoerner()
+	}
 }
