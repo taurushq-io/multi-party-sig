2deaa517b41ce586f866b923fc482be6d4033986
zzyalbert <zzy.albert@163.com>
2022-10-27 16:42:35 +0800

    add unit tests of presign & presignOnline

--- protocols/cmp/presign/abort_test.go
+++ protocols/cmp/presign/abort_test.go
@@ -1,15 +1,24 @@
 package presign
 
 import (
+	"errors"
+	"sort"
 	"testing"
 
 	"github.com/cronokirby/safenum"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/taurusgroup/multi-party-sig/internal/elgamal"
 	"github.com/taurusgroup/multi-party-sig/internal/round"
 	"github.com/taurusgroup/multi-party-sig/internal/test"
 	"github.com/taurusgroup/multi-party-sig/pkg/party"
 	"github.com/taurusgroup/multi-party-sig/pkg/pool"
+)
+
+var (
+	oneNat      = new(safenum.Nat).SetUint64(1)
+	oneInt      = new(safenum.Int).SetNat(oneNat)
+	minusOneInt = new(safenum.Int).SetNat(oneNat).Neg(1)
 )
 
 type TestRule struct {
@@ -45,9 +54,137 @@ func (tr *TestRule) ModifyContent(rNext round.Session, to party.ID, content roun
 	}
 }
 
-func TestRoundFail(t *testing.T) {
+// failRule defines the rules that will abort at step 6 or step 7
+type failRule struct {
+	failedParites map[party.ID]struct{}
+	changeStep    round.Number
+	changeAfter   func(round.Session)
+	changeContent func(round.Session, party.ID, round.Content)
+	abortErr      string
+}
+
+func (f *failRule) ModifyBefore(r round.Session) {}
+
+func (f *failRule) ModifyAfter(rNext round.Session) {
+	if !f.hitRule(rNext.Number()-1, rNext.SelfID()) || f.changeAfter == nil {
+		return
+	}
+	f.changeAfter(rNext)
+}
+
+func (f *failRule) ModifyContent(rNext round.Session, to party.ID, content round.Content) {
+	if !f.hitRule(rNext.Number()-1, rNext.SelfID()) || f.changeContent == nil {
+		return
+	}
+	f.changeContent(rNext, to, content)
+}
+
+func (f *failRule) HitParty(partyId party.ID) bool {
+	_, hit := f.failedParites[partyId]
+	return hit
+}
+
+func (f *failRule) GetCorruptPartyIDsAndAbortErr() ([]party.ID, string) {
+	partyIDs := make(party.IDSlice, 0, len(f.failedParites))
+	for key, _ := range f.failedParites {
+		partyIDs = append(partyIDs, key)
+	}
+	sort.Sort(partyIDs)
+	return partyIDs, f.abortErr
+}
+
+func (f *failRule) hitRule(step round.Number, partyId party.ID) bool {
+	if step != f.changeStep {
+		return false
+	}
+	return f.HitParty(partyId)
+}
+
+// newFailRule makes rules to fail the presign process at given 'step',
+// and adversarial index are given by 'failedParties'
+func newFailRule(step int, allParties party.IDSlice, failedParties ...int) (*failRule, error) {
+	failedPartyIDs := make(map[party.ID]struct{})
+	for _, idx := range failedParties {
+		id := allParties[idx]
+		failedPartyIDs[id] = struct{}{}
+	}
+	var (
+		changeStep    round.Number
+		changeAfter   func(round.Session)
+		changeContent func(round.Session, party.ID, round.Content)
+		abortErr      string
+	)
+	switch step {
+	case 6:
+		// in this case, we broadcast the wrong delta value at step 3
+		changeStep = 3
+		changeContent = func(rNext round.Session, to party.ID, content round.Content) {
+			body, ok := content.(*broadcast4)
+			if !ok || body == nil {
+				return
+			}
+			body.DeltaShare = body.DeltaShare.Add(group.NewScalar().SetNat(oneNat))
+		}
+		abortErr = "abort1: detected culprit"
+		break
+	case 7:
+		// in this case, we generate the wrong chi value and its ElGamal commitment at step 3
+		// and broadcast the wrong ElGamal conmmitment of chi
+		changeStep = 3
+		changeAfter = func(rNext round.Session) {
+			realRound, ok := rNext.(*presign4)
+			if !ok || realRound == nil {
+				return
+			}
+			realRound.ChiShare = realRound.ChiShare.Add(group.NewScalar().SetNat(oneNat))
+			ElGamalChi, ElGamalChiNonce := elgamal.Encrypt(realRound.ElGamal[realRound.SelfID()], realRound.ChiShare)
+			realRound.ElGamalChiNonce = ElGamalChiNonce
+			realRound.ElGamalChi = map[party.ID]*elgamal.Ciphertext{realRound.SelfID(): ElGamalChi}
+		}
+		changeContent = func(rNext round.Session, to party.ID, content round.Content) {
+			body, ok := content.(*broadcast4)
+			if !ok || body == nil {
+				return
+			}
+			realRound, ok := rNext.(*presign4)
+			if !ok || realRound == nil {
+				return
+			}
+			body.ElGamalChi = realRound.ElGamalChi[realRound.SelfID()]
+		}
+		abortErr = "abort2: detected culprit"
+		break
+	default:
+		return nil, errors.New("Unknow fail step to generate rule")
+	}
+	return &failRule{
+		failedParites: failedPartyIDs,
+		changeStep:    changeStep,
+		changeAfter:   changeAfter,
+		changeContent: changeContent,
+		abortErr:      abortErr,
+	}, nil
+}
+
+func checkAbortOutput(t *testing.T, output []round.Session, rule *failRule) {
+	expectedCulprits, abortErr := rule.GetCorruptPartyIDsAndAbortErr()
+	for _, r := range output {
+		require.IsType(t, &round.Abort{}, r, "expected abort round")
+		abortRound := r.(*round.Abort)
+		assert.EqualError(t, abortRound.Err, abortErr)
+		if !rule.HitParty(abortRound.SelfID()) {
+			assert.Equal(t, expectedCulprits, abortRound.Culprits)
+		}
+	}
+}
+
+func TestCommonFail(t *testing.T) {
 	pl := pool.NewPool(0)
 	defer pl.TearDown()
+
+	N := 4
+	T := N - 1
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, T, N, T, pl)
 
 	tests := []struct {
 		name string
@@ -139,4 +276,104 @@ func TestRoundFail(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestPresignT1N2FailStep6CorruptParty0(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	rule, err := newFailRule(6, partyIDs, 0)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT1N2FailStep6CorruptParty1(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	rule, err := newFailRule(6, partyIDs, 1)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT1N2FailStep6CorruptParty01(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	rule, err := newFailRule(6, partyIDs, 0, 1)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT2N5FailStep6CorruptParty0(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 2, 5, 3, pl)
+	rule, err := newFailRule(6, partyIDs, 0)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT2N5FailStep6CorruptParty02(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 2, 5, 3, pl)
+	rule, err := newFailRule(6, partyIDs, 0, 2)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT1N2FailStep7CorruptParty0(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	rule, err := newFailRule(7, partyIDs, 0)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT1N2FailStep7CorruptParty1(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	rule, err := newFailRule(7, partyIDs, 1)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT1N2FailStep7CorruptParty01(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	rule, err := newFailRule(7, partyIDs, 0, 1)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT2N5FailStep7CorruptParty0(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 2, 5, 3, pl)
+	rule, err := newFailRule(7, partyIDs, 0)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
+}
+
+func TestPresignT2N5FailStep7CorruptParty02(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, _ := generateTNPartiesAndKeys(t, 2, 5, 3, pl)
+	rule, err := newFailRule(7, partyIDs, 0, 2)
+	assert.NoError(t, err)
+	output := presignWithTNParties(t, configs, partyIDs, rule, nil, pl)
+	checkAbortOutput(t, output, rule)
 }
--- protocols/cmp/presign/sign_test.go
+++ protocols/cmp/presign/sign_test.go
@@ -4,7 +4,6 @@ import (
 	mrand "math/rand"
 	"testing"
 
-	"github.com/cronokirby/safenum"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/taurusgroup/multi-party-sig/internal/round"
@@ -18,53 +17,83 @@ import (
 )
 
 var (
-	oneNat      = new(safenum.Nat).SetUint64(1)
-	oneInt      = new(safenum.Int).SetNat(oneNat)
-	minusOneInt = new(safenum.Int).SetNat(oneNat).Neg(1)
-
-	N           = 4
-	T           = N - 1
 	group       = curve.Secp256k1{}
-	configs     map[party.ID]*config.Config
-	partyIDs    party.IDSlice
 	messageHash []byte
 )
 
 func init() {
-	source := mrand.New(mrand.NewSource(1))
-	pl := pool.NewPool(0)
-	defer pl.TearDown()
-	configs, partyIDs = test.GenerateConfig(group, N, T, source, pl)
-	for id, c := range configs {
-		configs[id], _ = c.DeriveBIP32(0)
-	}
-
 	messageHash = make([]byte, 64)
 	sha3.ShakeSum128(messageHash, []byte("hello"))
 }
 
-func TestRound(t *testing.T) {
+// generateTNPartiesAndKeys generates parties and their keys with T-N scheme,
+// and P parties will participate in signning
+func generateTNPartiesAndKeys(t *testing.T, T, N, P int, pl *pool.Pool) (map[party.ID]*config.Config, party.IDSlice, curve.Point) {
+	defer t.Logf("generated configs, T=%d, N=%d", T, N)
+	configs, partyIDs := test.GenerateConfig(group, N, T, mrand.New(mrand.NewSource(1)), pl)
+	for id, c := range configs {
+		configs[id], _ = c.DeriveBIP32(0)
+	}
+	publicKey := configs[partyIDs[0]].PublicPoint()
+	return configs, partyIDs[:P], publicKey
+}
 
-	rounds := make([]round.Session, 0, N)
-	for _, c := range configs {
-		pl := pool.NewPool(1)
-		defer pl.TearDown()
-		r, err := StartPresign(c, partyIDs, messageHash, pl)(nil)
+func presignWithTNParties(t *testing.T, configs map[party.ID]*config.Config, partyIDs party.IDSlice, rule test.Rule, message []byte, pl *pool.Pool) []round.Session {
+	rounds := make([]round.Session, 0, len(partyIDs))
+	for _, partyID := range partyIDs {
+		c := configs[partyID]
+		r, err := StartPresign(c, partyIDs, message, pl)(nil)
 		require.NoError(t, err, "round creation should not result in an error")
 		rounds = append(rounds, r)
 	}
-
 	for {
-		err, done := test.Rounds(rounds, nil)
+		err, done := test.Rounds(rounds, rule)
 		require.NoError(t, err, "failed to process round")
 		if done {
 			break
 		}
 	}
-	for _, r := range rounds {
-		assert.IsType(t, &round.Output{}, r)
-		signature, ok := r.(*round.Output).Result.(*ecdsa.Signature)
-		assert.True(t, ok, "result should *ecdsa.Signature")
-		assert.True(t, signature.Verify(configs[r.SelfID()].PublicPoint(), messageHash))
+	return rounds
+}
+
+func verifyOutput(t *testing.T, publicKey curve.Point, output []round.Session) {
+	for _, r := range output {
+		require.IsType(t, &round.Output{}, r, "expected result round")
+		resultRound := r.(*round.Output)
+		require.IsType(t, &ecdsa.Signature{}, resultRound.Result, "expected taproot signature result")
+		signature := resultRound.Result.(*ecdsa.Signature)
+		assert.True(t, signature.Verify(publicKey, messageHash), "expected valid signature")
 	}
+}
+
+func TestPresignAndPresignOnlineT1N2(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, publicKey := generateTNPartiesAndKeys(t, 1, 2, 2, pl)
+	output := presignWithTNParties(t, configs, partyIDs, nil, messageHash, pl)
+	verifyOutput(t, publicKey, output)
+}
+
+func TestPresignAndPresignOnlineT2N5(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, publicKey := generateTNPartiesAndKeys(t, 2, 5, 3, pl)
+	output := presignWithTNParties(t, configs, partyIDs, nil, messageHash, pl)
+	verifyOutput(t, publicKey, output)
+}
+
+func TestPresignAndPresignOnlineT5N6(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, publicKey := generateTNPartiesAndKeys(t, 5, 6, 6, pl)
+	output := presignWithTNParties(t, configs, partyIDs, nil, messageHash, pl)
+	verifyOutput(t, publicKey, output)
+}
+
+func TestPresignAndPresignOnlineT4N8(t *testing.T) {
+	pl := pool.NewPool(0)
+	defer pl.TearDown()
+	configs, partyIDs, publicKey := generateTNPartiesAndKeys(t, 4, 8, 5, pl)
+	output := presignWithTNParties(t, configs, partyIDs, nil, messageHash, pl)
+	verifyOutput(t, publicKey, output)
 }
