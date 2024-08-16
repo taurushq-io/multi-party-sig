6b81d1c7829381a70650cca2a1b2f5939f683eed
mpetrun5 <matija.petrunic@gmail.com>
2024-08-16 16:56:05 +0200

    Fix duplicate error message

--- internal/round/error.go
+++ internal/round/error.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	ErrNilFields      = errors.New("message contained empty fields")
 	ErrInvalidContent = errors.New("content is not the right type")
-	ErrOutChanFull    = errors.New("content is not the right type")
+	ErrOutChanFull    = errors.New("out channel is full")
 )
